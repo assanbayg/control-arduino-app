d8d4c8b61438284c728b2e2dd0d26ff07bc20170
Gauhar <asanbayg@gmail.com>
2024-07-10 19:36:26 +0500

    upload data to firebase database

--- arduino/main.ino
+++ arduino/main.ino
@@ -4,12 +4,16 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define FIREBASE_HOST "FIREBASE_DATABASE_URL"
-#define FIREBASE_AUTH "FIREBASE_API_KEY"
+// Replace with your wifi settings
 #define WIFI_SSID "Teachers_WIFI"
 #define WIFI_PASSWORD "Nis12345"
 
-String values, sensor_data;
+// Replace this too
+#define FIREBASE_HOST "FIREBASE_DATABASE_URL"
+#define FIREBASE_AUTH "FIREBASE_API_KEY"
+#define FIREBASE_CONTROL_REF "/device/control"
+
+FirebaseData firebaseData;
 
 void setup(){
   Serial.begin(9600); 
@@ -19,88 +23,78 @@ void setup(){
   while (WiFi.status() != WL_CONNECTED) 
   {
     delay(500);
+    Serial.println(".");
   }
+
+  Serial.println();
+  Serial.println("Connected to Wi-Fi");
+
+  // Connect to Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
+  Firebase.reconnectWiFi(true);
 }
+
 void loop() {
+  // Create a JSON object to hold the data
+  String jsonData = "{";
+  jsonData += "\"temperature\": ";
+  jsonData += "55";
+  jsonData += ", \"humidity\": ";
+  jsonData += "a lot";
+  jsonData += "}";
 
- bool Sr =false;
- 
-  while(Serial.available()){
-    sensor_data=Serial.readString(); 
-    Sr=true;    
-    }
-  
-delay(1000);
-
-  if(Sr==true){  
-    
-  values=sensor_data;
-  
-
-  int fristCommaIndex = values.indexOf(',');
-  int secondCommaIndex = values.indexOf(',', fristCommaIndex+1);
-  int thirdCommaIndex = values.indexOf(',', secondCommaIndex + 1);
-  
- 
-  String ultrasonic_value = values.substring(0, fristCommaIndex);
-
-  Firebase.setString("ultrasonic_value",ultrasonic_value);
-   delay(10);
-
-
-  delay(10);
-  
-  
-  Firebase.pushString("previous_ultrasonic_value",ultrasonic_value);
-   
-  
-  
-  delay(1000);
+  // Upload the data to Firebase
+  if (Firebase.pushJSON(firebaseData, FIREBASE_CONTROL_REF, jsonData)) {
+    Serial.println("Data uploaded successfully");
+  } else {
+    Serial.print("Failed to upload data, reason: ");
+    Serial.println(firebaseData.errorReason());
+  }
   
   if (Firebase.failed()) {  
       return;
   }
   
+
+  delay(5000);
 }   
-}
 
 
-// ●	Далее мы загружаем код в саму Arduino для работы ультразвукового датчика:
-int trigger_pin = 2;
-int echo_pin = 3;
+// // ●	Далее мы загружаем код в саму Arduino для работы ультразвукового датчика:
+// int trigger_pin = 2;
+// int echo_pin = 3;
 
-int time;
-int distance; 
-String value;
+// int time;
+// int distance; 
+// String value;
 
-void setup()
-{
-  pinMode (trigger_pin, OUTPUT); 
-  pinMode (echo_pin, INPUT);
-  Serial.begin(9600); 
-  delay(2000);  
-}
+// void setup()
+// {
+//   pinMode (trigger_pin, OUTPUT); 
+//   pinMode (echo_pin, INPUT);
+//   Serial.begin(9600); 
+//   delay(2000);  
+// }
 
  
-void loop()
-{
-  value = get_distance_Value();
-  delay(1000);
-  Serial.flush();
-  delay(1000);
-  Serial.print(value);
-  delay(2000);
-}
+// void loop()
+// {
+//   value = get_distance_Value();
+//   delay(1000);
+//   Serial.flush();
+//   delay(1000);
+//   Serial.print(value);
+//   delay(2000);
+// }
 
-String get_distance_Value()
-{
-  digitalWrite(trigger_pin, 1);
-  delayMicroseconds(10);
-  digitalWrite(trigger_pin, 0);
-  time = pulseIn(echo_pin, 1);
+// String get_distance_Value()
+// {
+//   digitalWrite(trigger_pin, 1);
+//   delayMicroseconds(10);
+//   digitalWrite(trigger_pin, 0);
+//   time = pulseIn(echo_pin, 1);
 
-  distance = (time * 34) / 2;
-  return String(distance);  
-}
+//   distance = (time * 34) / 2;
+//   return String(distance);  
+// }
 
